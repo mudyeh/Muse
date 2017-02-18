e56016b28a4756df3d4412b66f51066fe8b89def
mudyeh <mattyeh987@gmail.com>
2017-02-18 08:41:20 -0800

    Wrote first iteration of melodic generator (most likely will turn into a default inherited method)

--- BluesGenerator.java
+++ BluesGenerator.java
@@ -6,16 +6,21 @@ import org.jfugue.theory.*; // sloppy wild card import
 import org.jfugue.pattern.*;
 import org.jfugue.player.*;
 
+import java.util.List;
+
 //import javax.sound.midi.Instrument;
 
 public class BluesGenerator {
-    public int measuresInForm;
-    public int tempo;
-    public String backingInstrument;
-    public String leadInstrument;
-    public String root;
+    private int measuresInForm;
+    private int tempo;
+    private String backingInstrument;
+    private String leadInstrument;
+    private String root;
 
-    public static int voiceNum = 0;
+    private int voiceNum = 0;
+    private int subBeatCount = 0;
+    private static final int LOWEST_DIVISION = 32;
+    private TimeSignature ts = new TimeSignature(4, 4);
 //    public String chordProgression;
 
 
@@ -50,10 +55,98 @@ public class BluesGenerator {
         }
     }
 
+    // at some point should also return rests as well ***
+    private String getRandNote() {
+        int noteNum = StdRandom.uniform(0, 12);
+        switch (noteNum) {
+            case 0:
+                return "C";
+            case 1:
+                return "C#";
+            case 2:
+                return "D";
+            case 3:
+                return "D#";
+            case 4:
+                return "E";
+            case 5:
+                return "F";
+            case 6:
+                return "F#";
+            case 7:
+                return "G";
+            case 8:
+                return "G#";
+            case 9:
+                return "A";
+            case 10:
+                return "Bb";
+            case 11:
+                return "B";
+            default:
+                return null;
+        }
+    }
+
+    /* maximum number of options (for now) is 6: whole to 32nd
+    * Lowering this number is not a linear process: lowest/highest values aren't
+    * necessarily removed first. */
+    private String getDurationNum(int numOptions) {
+        int durationNum = StdRandom.uniform(0, numOptions);
+        switch (durationNum) {
+            case 0:
+                subBeatCount += 8;
+                return "q";
+            case 1:
+                subBeatCount += 4;
+                return "i";
+            case 2:
+                subBeatCount += 2;
+                return "s";
+            case 3:
+                subBeatCount += 16;
+                return "h";
+            case 4:
+                subBeatCount += 1;
+                return "t";
+            case 5:
+                subBeatCount += 32;
+                return "w";
+            default:
+                return null;
+        }
+    }
+
+    // inclusive
+    private int getOctaveNum(int min, int max) {
+        return StdRandom.uniform(min, max+1);
+    }
+
     /* The more "computer music" melody generator:
-     * Randomly selects diatonic notes for melody */
+     * Randomly selects notes from chromatic scale for melody */
     public Pattern getRandomLeadPattern() {
-        return null;
+        int totalSubBeats = LOWEST_DIVISION
+                * measuresInForm
+                * ts.getBeatsPerMeasure()
+                / ts.getDurationForBeat();
+        String songString = root
+                + getOctaveNum(3, 5)
+                + getDurationNum(4)
+                + " ";
+        // currently hardcode what the ranges/numOptions are; will later add user ability to manipulate
+        while (subBeatCount < totalSubBeats) {
+            songString = songString.concat(getRandNote()
+                    + getOctaveNum(3, 5)
+                    + getDurationNum(4)
+                    + " ");
+        }
+        Pattern lead = new Pattern(songString)
+                .setVoice(voiceNum)
+                .setTempo(tempo)
+                .setInstrument(leadInstrument);
+        voiceNum += 1;
+
+        return lead;
     }
 
     /* The more "human" melody generator:
@@ -92,13 +185,17 @@ public class BluesGenerator {
     }
 
     public static void main(String[] args) {
-        BluesGenerator b = new BluesGenerator(12, 140, "G",
+        BluesGenerator b = new BluesGenerator(16, 160, "C",
                 "electric_jazz_guitar", "overdriven_guitar");
 //        System.out.print(b.determineForm()); //do more extensive testing later!!!
 
         Player p = new Player();
-        p.play(b.getBackingPattern());
-//        Pattern pat = new Intervals("1 3 5").setRoot("D").getPattern();
+        Pattern rhythm = b.getBackingPattern();
+        Pattern lead = b.getRandomLeadPattern();
+        p.play(rhythm.add(lead));
+//        Pattern pat = new Intervals("1 3 5 9 11").setRoot("C").getPattern();
 //        System.out.print(pat);
+//        List<Note> s = new Intervals("1 5 10 15 17").setRoot("D3").getNotes();
+//        System.out.print(s);
     }
 }
